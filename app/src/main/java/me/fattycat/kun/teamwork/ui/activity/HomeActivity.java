b85354e86481794e31ac991bcc6657d2448a789e
kelvin <qxm329232741@gmail.com>
2016-04-09 17:26:14 +0800

    Fix file name problem.

---
+++ app/src/main/java/me/fattycat/kun/teamwork/ui/activity/HomeActivity.java
@@ -0,0 +1,132 @@
+/*
+ * TeamWork
+ * Copyright (C) 2015  FattycatR
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program;  if not, see <http://www.gnu.org/licenses/>.
+ */
+package me.fattycat.kun.teamwork.ui.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import me.fattycat.kun.teamwork.R;
+import me.fattycat.kun.teamwork.ui.fragment.TeamFragment;
+import me.fattycat.kun.teamwork.ui.fragment.UserFragment;
+
+public class HomeActivity extends BaseActivity {
+    private static final String TAG = "TW_HomeActivity";
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+    @Bind(R.id.tabs)
+    TabLayout mTabLayout;
+    @Bind(R.id.container)
+    ViewPager mViewPager;
+    @Bind(R.id.fab)
+    FloatingActionButton mFab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        mToolbar.setTitle(getString(R.string.app_name));
+        setSupportActionBar(mToolbar);
+
+        initTabs();
+        initFab();
+
+    }
+
+    private void initFab() {
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void initTabs() {
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        sectionsPagerAdapter.addFragment(new TeamFragment(), "团队");
+        sectionsPagerAdapter.addFragment(new UserFragment(), "我");
+        sectionsPagerAdapter.addFragment(new UserFragment(), "我");
+        sectionsPagerAdapter.addFragment(new UserFragment(), "我");
+        mViewPager.setAdapter(sectionsPagerAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mTitles = new ArrayList<>();
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles.get(position);
+        }
+    }
+}
---
+++ app/src/main/java/me/fattycat/kun/teamwork/ui/activity/LoginActivity.java
@@ -0,0 +1,111 @@
+package me.fattycat.kun.teamwork.ui.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+
+import com.roger.match.library.MatchButton;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import me.fattycat.kun.teamwork.R;
+import me.fattycat.kun.teamwork.TWAccessToken;
+import me.fattycat.kun.teamwork.TWApi;
+import me.fattycat.kun.teamwork.event.AuthorizeEvent;
+import me.fattycat.kun.teamwork.util.LogUtils;
+import me.fattycat.kun.teamwork.util.ToastUtils;
+
+public class LoginActivity extends BaseActivity {
+    private static final String TAG = "TW_LoginActivity";
+
+    @Bind(R.id.login)
+    MatchButton mBtnLogin;
+
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mContext = this;
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        loadAuthorization();
+        checkAuthorization();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    private void checkAuthorization() {
+        if (TWAccessToken.sIsAuthorized) {
+            mBtnLogin.setVisibility(View.GONE);
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+
+            LogUtils.i(TAG, "checkAuthorization | authorized");
+        } else {
+            mBtnLogin.setVisibility(View.VISIBLE);
+
+            LogUtils.i(TAG, "checkAuthorization | not authorized");
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        LogUtils.i(TAG, "onNewIntent | not authorized");
+
+        getAccessToken(intent);
+    }
+
+    @OnClick(R.id.login)
+    public void login() {
+        Intent loginIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(TWApi.OAUTHURL));
+        startActivity(loginIntent);
+    }
+
+    // Chrome Custom Tabs
+    //@OnClick(R.id.login)
+    public void requestAuth() {
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        builder.setToolbarColor(ContextCompat.getColor(mContext, R.color.colorPrimary));
+        builder.build().launchUrl(this, Uri.parse(TWApi.OAUTHURL));
+    }
+
+    @Subscribe
+    public void onAuthorize(AuthorizeEvent event) {
+
+        LogUtils.i(TAG, "onAuthorize");
+
+        if (event.isAuthorized) {
+            loadAuthorization();
+            checkAuthorization();
+        } else {
+            // FIXME: 16/3/16 test
+            ToastUtils.showShort("授权失败");
+        }
+    }
+}
